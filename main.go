d10dd3bb67e27686a048731c2ea29d6799ae811a
Steffen Müthing <steffen.muething@iwr.uni-heidelberg.de>
2020-04-19 12:00:51 +0200

    Call goroutine shutdown from main

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"time"
+
+	"github.com/smuething/devicemonitor/app"
 	"github.com/smuething/devicemonitor/ui"
 )
 
 func main() {
 	ui.RunUI()
+	shutdownCtx, cancelShutdown := app.ContextWithTimeout(time.Second, false)
+	defer cancelShutdown()
+	if !app.Shutdown(shutdownCtx) {
+		ui.ShowError(nil, "Error", "Timeout during shutdown")
+	}
 }
