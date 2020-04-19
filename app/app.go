4394840d8d136d27b0fa54fb4248577ad74a220e
Steffen Müthing <steffen.muething@iwr.uni-heidelberg.de>
2020-04-19 12:00:00 +0200

    Add application package
    The application package provides global infrastructure. Right now this includes:
    
    - a global WaitGroup with helper functions for lauching goroutines and
      cleaning up.
    - A global, cancellable context for signalling shutdown to goroutines

---
+++ app/app.go
@@ -0,0 +1,55 @@
+package app
+
+import (
+	"context"
+	"sync"
+	"time"
+)
+
+var wg *sync.WaitGroup = &sync.WaitGroup{}
+var backgroundCtx context.Context
+var ctx context.Context
+var cancel context.CancelFunc
+
+func init() {
+	backgroundCtx = context.Background()
+	ctx, cancel = context.WithCancel(backgroundCtx)
+}
+
+func Go(f func()) {
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		f()
+	}()
+}
+
+func Shutdown(ctx context.Context) bool {
+
+	cancel()
+
+	c := make(chan struct{})
+	go func() {
+		defer close(c)
+		wg.Wait()
+	}()
+
+	select {
+	case <-c:
+		return true
+	case <-ctx.Done():
+		return false
+	}
+}
+
+func Context() context.Context {
+	return ctx
+}
+
+func ContextWithTimeout(timeout time.Duration, nested bool) (context.Context, context.CancelFunc) {
+	if nested {
+		return context.WithTimeout(ctx, timeout)
+	} else {
+		return context.WithTimeout(backgroundCtx, timeout)
+	}
+}
